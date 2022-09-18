bb13e65124a69e27aa76f9000937c54c828942bb
GeraldSalazar <geraldsalazar4@gmail.com>
2022-09-18 09:46:07 -0600

    random instruct builder, according to Poisson PD

---
+++ src/app/instruction-building/instruction-builder.ts
@@ -0,0 +1,28 @@
+
+import { Instruction } from './instruction-interface'
+import { generateRandomDataToWrite } from './instruction-units/random-data-gen';
+import { generateRandomDir } from './instruction-units/random-dir-gen';
+import { generateRandomOperation } from './instruction-units/random-operation-gen';
+
+enum Operations {
+    calcOp = 'CALC',
+    readOp = 'READ',
+    writeOp = 'WRITE'
+}
+
+export function buildInstruction(cpuNum: number){
+    //Instruction Format: {cpuNum: number, op: string, dir: string, data: string}
+    //Example Instruction: {cpuNum: number = 1, op: string = 'WRITE', dir: string = '001', data: string = '0xABCD'}
+    const operation = generateRandomOperation();
+    let instruction: Instruction = {cpuNum, op: operation}
+    
+    const dir = generateRandomDir();
+    const data = generateRandomDataToWrite();
+    if(operation !== Operations.calcOp){
+        instruction.dir = dir;
+    }
+    if(operation === Operations.writeOp){
+        instruction.data = data
+    }
+    return instruction
+}
